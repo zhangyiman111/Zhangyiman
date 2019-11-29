3d2b31ffe6675c3da94d7fe6eb40d7c3fc1f7a3c
zhangyiman <841589912@qq.com>
2019-11-29 16:28:14 +0800

    张奕漫

---
+++ app/src/main/java/com/bawei/myapplication/MainActivity.java
@@ -0,0 +1,19 @@
+package com.bawei.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //注释
+        //张奕漫
+
+
+
+    }
+}
